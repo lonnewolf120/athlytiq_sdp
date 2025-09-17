68c9982f77b16eb21588b6e78c9e84f3c273570c
iftekharul Islam <iftekharulislam360@gmail.com>
2025-09-17 07:27:52 +0600

    Changes to be committe

--- fitnation/lib/services/notification_service.dart
+++ fitnation/lib/services/notification_service.dart
@@ -114,8 +114,6 @@ class NotificationService {
         ),
       ),
       androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
       payload: payload,
     );
 
